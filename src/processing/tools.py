ca49163ebb8169f687cb29d5525362967a4f9e7b
Sasi Kiran Gaddipati <gsasikiran000@gmail.com>
2019-11-07 13:34:28 +0100

    Add requirements and Change VGG16 file name

---
+++ src/processing/tools.py
@@ -0,0 +1,22 @@
+
+class Tools:
+    def __init__(self):
+        pass
+
+    def get_conv_layers(self, model):
+        '''
+            This method returns the list of convolutional layers' numbers.
+        :param model: Model architecture
+            A neural network model with a stack of layers
+        :return: list
+            Returns a list of integers with convolutional layers
+        '''
+        conv_layer_list = []
+        for i in range(len(model.layers)):
+            layer = model.layers[i]
+            # check for convolutional layer
+            if 'conv' not in layer.name:
+                continue
+            # summarize output shape
+            conv_layer_list.append(i)
+        return conv_layer_list
