ac5e7b125d0fa16ff7c8246abd5821eb1d0a06b5
Sasi Kiran Gaddipati <gsasikiran000@gmail.com>
2019-12-04 13:59:42 +0100

    Add tests

--- src/test/test.py
+++ src/test/test.py
@@ -1,23 +1,49 @@
 import unittest
+
 import numpy as np
 
-
-class Output(unittest.TestCase):
-
-    def test_string(self):
-        output = "class name"
-        assert type(output) == str
-
-    def test_class(self):
-        pass
+from src.pre_processing.read_data import Data
 
 
-class TestActivationPattern(unittest.TestCase):
+class Image(unittest.TestCase):
 
-    def test_shape(self):
-        saved_activation = np.array([[1, 2], [3, 4]])
-        test_activation = np.array([[1, 3], [2, 4]]).T
-        self.assertTrue((saved_activation == test_activation).all())
+    # def __init__(self):
+    #     super().__init__()
+    #     self.MNIST_data = Data()
+    #     self.x_train, self.y_train = self.MNIST_data.get_data()
+    #     self.x_train_images = self.MNIST_data.df_to_images(self.x_train)
+
+    # def image_type(self):
+    #     pass
+
+    def image_shape(self):
+        MNIST_data = Data()
+        x_train, y_train = MNIST_data.get_data()
+        x_train_images = MNIST_data.df_to_images(x_train)
+
+        for image in x_train_images:
+            self.assertEqual(np.shape(image), (28, 28))
+
+
+# class Output(unittest.TestCase):
+#
+#     def __init__(self):
+#         super().__init__()
+#
+#     def test_string(self):
+#         output = "class name"
+#         assert type(output) == str
+#
+#     def test_class(self):
+#         pass
+#
+#
+# class TestActivationPattern(unittest.TestCase):
+#
+#     def test_shape(self):
+#         saved_activation = np.array([[1, 2], [3, 4]])
+#         test_activation = np.array([[1, 3], [2, 4]]).T
+#         self.assertTrue((saved_activation == test_activation).all())
 
 
 if __name__ == "__main__":
