5a283f132754b6d414ade97ff6b9fd0ba1b7f420
Sasi Kiran Gaddipati <gsasikiran000@gmail.com>
2019-12-11 18:55:30 +0100

    Added inputs

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+import warnings
+
 from keras.models import Model
-import matplotlib.pyplot as plt
 
-
+from src.post_processing.visualization import DisplayActivations
+from src.pre_processing.read_data import Data
 from src.processing.model_architecture import ModelArchitecture
 from src.processing.tools import Tools
-from src.pre_processing.read_data import Data
-from src.post_processing.visualization import DisplayActivations
+
+warnings.filterwarnings("ignore")
 
 # img = image_preprocess_tri("/home/gsasikiran/Desktop/Semester_3/SDP/Monitoring-Neuron-Activations/triangles/drawing(1).png")
 
@@ -24,10 +26,11 @@ conv_layer_list = tools.get_conv_layers(model_cnn)
 
 outputs = [model_cnn.layers[i].output for i in conv_layer_list]
 model = Model(inputs=model_cnn.inputs, outputs=outputs)
-img = X_train_images[50]
+
+img_num = int(input('Input some number'))
+img = X_train_images[img_num]
 feature_maps = model.predict(img.reshape(1, 28, 28, 1))
 
+layer_num = int(input('Input layer number to be plotted.. (If NONE plots all the layer activations)'))
 display_activation = DisplayActivations()
-
-display_activation.plot_activation(feature_maps)
-
+display_activation.plot_activation(feature_maps, layer_num)
