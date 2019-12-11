6f7fc649d23264a34cf3238cc4b49500493943d9
Sasi Kiran Gaddipati <gsasikiran000@gmail.com>
2019-12-11 18:37:38 +0100

    Program to run particular layer

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from keras.models import Model
+import matplotlib.pyplot as plt
+
 
 from src.processing.model_architecture import ModelArchitecture
 from src.processing.tools import Tools
 from src.pre_processing.read_data import Data
+from src.post_processing.visualization import DisplayActivations
 
 # img = image_preprocess_tri("/home/gsasikiran/Desktop/Semester_3/SDP/Monitoring-Neuron-Activations/triangles/drawing(1).png")
 
@@ -21,26 +24,10 @@ conv_layer_list = tools.get_conv_layers(model_cnn)
 
 outputs = [model_cnn.layers[i].output for i in conv_layer_list]
 model = Model(inputs=model_cnn.inputs, outputs=outputs)
-img = X_train_images[5]
+img = X_train_images[50]
 feature_maps = model.predict(img.reshape(1, 28, 28, 1))
 
-print(feature_maps)
+display_activation = DisplayActivations()
 
-# display_activation(feature_maps,8,4,2)
+display_activation.plot_activation(feature_maps)
 
-# row = 8
-# col = 4
-# for fmap in feature_maps:
-#
-#     ix = 1
-#     for _ in range(row):
-#         for _ in range(col):
-#             ax = plt.subplot(row, col, ix)
-#             ax.set_xticks([])
-#             ax.set_yticks([])
-#
-#             plt.imshow(fmap[0, :, :, ix-1], cmap='gray')
-#
-#             ix += 1
-#
-#     plt.show()
---
+++ src/post_processing/visualization.py
@@ -0,0 +1,81 @@
+import matplotlib.pyplot as plt
+
+
+class DisplayActivations:
+
+    def __init__(self):
+
+        pass
+
+    @staticmethod
+    def __get_rows_cols(n):
+        '''
+
+        '''
+        if n == 32:
+            return 8, 4
+        elif n == 64:
+            return 8, 8
+        elif n == 128:
+            return 8, 16
+        elif n == 256:
+            return 16, 16
+        elif n == 512:
+            return 16, 32
+
+    def plot_activation(self, feature_maps, layer=None):
+        """
+            Plot the activation layers of the predicted feature maps
+        @param
+        feature_maps: List
+            The list of activation outputs from the predicted layers
+        rows: int (default:8)
+            Number of rows to be in the plot
+        cols: int (default:4)
+            Number of cols to be in the plot
+        layer: int (default: None)
+            The layer of activation to be plotted. If none, plots all the layers
+
+        @returns
+            Returns a set of plots with assigned rows and cols
+        """
+
+        if not layer:
+            feature_maps = feature_maps
+
+            for fmap in feature_maps:
+
+                rows, cols = self.__get_rows_cols(len(fmap[0][0][1]))
+
+                ix = 1
+                for _ in range(rows):
+                    for _ in range(cols):
+                        ax = plt.subplot(rows, cols, ix)
+                        ax.set_xticks([])
+                        ax.set_yticks([])
+
+                        plt.imshow(fmap[0, :, :, ix - 1], cmap='gray')
+
+                        ix += 1
+
+                plt.show()
+
+        else:
+
+            feature_maps = feature_maps[layer - 1]
+
+            for fmap in feature_maps:
+
+                rows, cols = self.__get_rows_cols(len(fmap[0][1]))
+                ix = 1
+                for _ in range(rows):
+                    for _ in range(cols):
+                        ax = plt.subplot(rows, cols, ix)
+                        ax.set_xticks([])
+                        ax.set_yticks([])
+
+                        plt.imshow(fmap[:, :, ix - 1], cmap='gray')
+
+                        ix += 1
+
+                plt.show()
